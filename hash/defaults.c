a97caf31b18cbee8ce7793ddc2c16a782d31f0ee
Tonnerre Lombard <tonnerre@jules.pas-un-geek-en-tant-que-tel.ch>
2009-06-08 00:34:05 +0200

    Add the referenced default functions (hash, string equality, etc.).

---
+++ hash/defaults.c
@@ -0,0 +1,65 @@
+/*-
+ * Copyright (c) 2009 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Tonnerre Lombard <tonnerre@NetBSD.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "clib_internal.h"
+
+/**
+ * Compare two strings. In case of equality, return 1, otherwise 0.
+ */
+int
+c_stringequals(const void *a, const void *b)
+{
+	const char *str_a = a, *str_b = b;
+	return !strcmp(str_a, str_b);
+}
+
+/**
+ * Return a hash value of the given string.
+ */
+int
+c_stringhash(const void *input)
+{
+	const char *cvals = input;
+	uint32_t hval;
+	size_t i;
+
+	for (i = 0; i < strlen(input); i++)
+		hval += cvals[i];
+
+	return hval;
+}
+
+/**
+ * Return a dummy hash value of 0. Useful for linear hashmaps (size == 0).
+ */
+int
+c_dummyhash(const void *input)
+{
+	return 0;
+}
