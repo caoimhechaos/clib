faebbce3e3bbf48f63ecddaea2697af7973da195
Tonnerre Lombard <tonnerre@bsdprojects.net>
2009-06-14 02:27:43 +0200

    Add a special test macro for string equality which makes debugging the test output easier.

--- t/testsuite.h
+++ t/testsuite.h
@@ -8,4 +8,19 @@
 		fprintf(stderr, "Test %d: %s: FAIL (%s)\n", ++ntest,	\
 			text, #code); } while(0)
 
+#define	test_expect_str(text, code, value)	do { char *res = code;	\
+	if (res == NULL) {						\
+		fprintf(stderr, "Test %d: %s: FAIL (%s -> NULL)\n",	\
+			++ntest, text, #code);				\
+		exitval |= 1;						\
+		break;							\
+	}								\
+	if (!strcmp(res, value)) {					\
+		fprintf(stderr, "Test %d: %s: OK\n", ++ntest, text);	\
+	} else								\
+		fprintf(stderr, "Test %d: %s: FAIL (\"%s\" != \"%s\")\n",\
+			++ntest, text, res, value);			\
+		exitval |= 1;						\
+	} while(0)
+
 #endif /* HAVE_T_TESTSUITE_H */
