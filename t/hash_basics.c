e802322269f574b754c16bd0d035c0978a1f6dba
Caoimhe Chaos <caoimhechaos@protonmail.com>
2009-06-14 02:27:43 +0200

    Add a special test macro for string equality which makes debugging the test output easier.

--- t/hash_basics.c
+++ t/hash_basics.c
@@ -18,16 +18,16 @@ main(void)
 		c_hashtable_insert(h, strdup("charly"), strdup("bacon")));
 
 	/* Fetch data. */
-	testresult("lookup hashtable 1",
-		c_stringequals(c_hashtable_lookup(h, "charly"), "bacon"));
+	test_expect_str("lookup hashtable 1",
+		c_hashtable_lookup(h, "charly"), "bacon");
 
 	/* Replace it. */
 	testresult("replace hashtable",
 		c_hashtable_replace(h, "charly", strdup("fries")));
 
 	/* Fetch it again. */
-	testresult("lookup hashtable 2",
-		c_stringequals(c_hashtable_lookup(h, "charly"), "fries"));
+	test_expect_str("lookup hashtable 2",
+		c_hashtable_lookup(h, "charly"), "fries");
 
 	/* Remove it all. */
 	testresult("clear hashtable", c_hashtable_remove_all(h));
