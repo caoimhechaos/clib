d2c6b3601790412029f482a0bf991e7e20a8cfb0
Lars Hartmann <lars@chaotika.org>
2009-06-14 22:44:52 +0200

    include stdint.h
    Signed-off-by: Tonnerre Lombard <tonnerre@NetBSD.ch>

--- clib_internal.h
+++ clib_internal.h
@@ -5,6 +5,9 @@
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
@@ -17,7 +20,6 @@
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
 #endif
-
 #ifdef HAVE_QUEUE_H
 #include <queue.h>
 #endif
