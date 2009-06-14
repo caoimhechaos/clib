8b67b2a23257da8b15d350bf080303a9be1175bd
Tonnerre Lombard <tonnerre@bsdprojects.net>
2009-06-14 02:33:22 +0200

    Add some more tests to unleash bugs in the replace function.

--- t/hash_basics.c
+++ t/hash_basics.c
@@ -14,20 +14,34 @@ main(void)
 		h = c_hashtable_new(c_stringhash, c_stringequals));
 
 	/* Insert data. */
-	testresult("insert hashtable",
+	testresult("insert hashtable 1",
 		c_hashtable_insert(h, strdup("charly"), strdup("bacon")));
+	testresult("insert hashtable 2",
+		c_hashtable_insert(h, strdup("charlene"), strdup("apple")));
 
 	/* Fetch data. */
 	test_expect_str("lookup hashtable 1",
 		c_hashtable_lookup(h, "charly"), "bacon");
+	test_expect_str("lookup hashtable 2",
+		c_hashtable_lookup(h, "charlene"), "apple");
 
 	/* Replace it. */
-	testresult("replace hashtable",
+	testresult("replace hashtable existing",
 		c_hashtable_replace(h, "charly", strdup("fries")));
 
 	/* Fetch it again. */
-	test_expect_str("lookup hashtable 2",
+	test_expect_str("lookup hashtable 3",
 		c_hashtable_lookup(h, "charly"), "fries");
+	test_expect_str("lookup hashtable 4",
+		c_hashtable_lookup(h, "charlene"), "apple");
+
+	/* Insert new data with replace. */
+	testresult("replace hashtable new",
+		c_hashtable_replace(h, strdup("elephant"), strdup("peanuts")));
+
+	/* Fetch that too. */
+	test_expect_str("lookup hashtable 5",
+		c_hashtable_lookup(h, "elephant"), "peanuts");
 
 	/* Remove it all. */
 	testresult("clear hashtable", c_hashtable_remove_all(h));
