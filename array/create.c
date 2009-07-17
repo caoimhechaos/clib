392ff53b457a222e29b7e87a6e045adc7b9ceb46
Tonnerre Lombard <Tonnerre.Lombard@SyGroup.ch>
2009-07-17 14:14:04 +0200

    Add implementation of dynamically resizing arrays.

---
+++ array/create.c
@@ -0,0 +1,63 @@
+/*-
+ * Copyright (c) 2009 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Tonnerre Lombard <tonnerre@NetBSD.org>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "clib_internal.h"
+
+/**
+ * Create a dynamically resizing array.
+ */
+struct c_array *
+c_array_new(c_resizestrategy strategy)
+{
+	struct c_array *a = malloc(sizeof(struct c_array));
+
+	if (!a) return NULL;
+
+	a->a_len = 0;
+	a->resizer = strategy;
+
+	a->a_size = strategy(0, 0);
+
+	a->a_values = malloc((a->a_size ? a->a_size : 1) * sizeof(void *));
+
+	return a;
+}
+
+/**
+ * Destroy the array again.
+ */
+void
+c_array_destroy(struct c_array *a)
+{
+	free(a->a_values);
+	free(a);
+}
