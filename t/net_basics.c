0580ed27922ad3877374fb3cce63492f6b36fe44
Caoimhe Chaos <caoimhechaos@protonmail.com>
2009-07-29 12:16:14 +0200

    Don't make the net_basics test leak memory, even though it is irrelevant.

--- t/net_basics.c
+++ t/net_basics.c
@@ -8,19 +8,26 @@ main(void)
 {
 	struct sockaddr_storage *sa;
 	int ntest = 0, exitval = 0, special;
+	void *ptr;
 	uint32_t ip;
 
 	test_expect_int(int, (special = c_str2sockaddr("127.0.0.1:53", &sa)), 0);\
 	if (special)
 		fprintf(stderr, "Error was: %s\n", gai_strerror(special));
 	test_expect_int(int, sa->ss_family, AF_INET);
-	test_expect_str("reserialized IP address", c_sockaddr2str(sa), "127.0.0.1");
+	test_expect_str("reserialized IP address", (ptr = c_sockaddr2str(sa)), "127.0.0.1");
+	if (ptr)
+		free(ptr);
+	free(sa);
 
 	test_expect_int(int, (special = c_str2sockaddr("[::1]:53", &sa)), 0);\
 	if (special)
 		fprintf(stderr, "Error was: %s\n", gai_strerror(special));
 	test_expect_int(int, sa->ss_family, AF_INET6);
-	test_expect_str("reserialized IP address", c_sockaddr2str(sa), "::1");
+	test_expect_str("reserialized IP address", (ptr = c_sockaddr2str(sa)), "::1");
+	if (ptr)
+		free(ptr);
+	free(sa);
 
 	exit(exitval ? EXIT_FAILURE : EXIT_SUCCESS);
 }
